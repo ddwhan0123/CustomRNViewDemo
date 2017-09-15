7942573f1b522b06a3f7fe43e45c1f6bad4a12e2
王佳杰 <526357367@qq.com>
2017-09-15 18:12:44 +0800

    添加package

--- android/app/src/main/java/com/customrnviewdemo/MainApplication.java
+++ android/app/src/main/java/com/customrnviewdemo/MainApplication.java
@@ -2,8 +2,7 @@ package com.customrnviewdemo;
 
 import android.app.Application;
 
-import com.customrnviewdemo.reactpackage.CustomViewPackage;
-import com.customrnviewdemo.view.CustomAndroidTextView;
+import com.customrnviewdemo.reactpackage.ViewPackage;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -25,7 +24,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
             return Arrays.<ReactPackage>asList(
                     new MainReactPackage(),
-                    new CustomViewPackage()
+                    new ViewPackage()
             );
         }
     };
